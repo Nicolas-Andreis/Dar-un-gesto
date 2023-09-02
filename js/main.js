f1420d5319347d06a3a3fb7d717a1f4aab81c5fa
Nicolas Andreis <jnandreis@outlook.com>
2023-09-02 17:01:43 -0300

    Primer commit

--- js/main.js
+++ js/main.js
@@ -8,3 +8,46 @@ buttons.forEach(button => {
         button.classList.add('active');
     });
 });
+
+
+//
+// Código para el modal "donar"
+// Obtén una referencia al botón que abre el modal
+const openModalButtons = document.querySelectorAll('[data-modal]');
+
+// Obtén una referencia al modal y al botón de cierre
+const modals = document.querySelectorAll('.modal');
+const closeModalButtons = document.querySelectorAll('.close');
+
+// Función para abrir el modal
+function openModal(event) {
+    const modalId = event.target.getAttribute('data-modal');
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+// Función para cerrar el modal
+function closeModal(event) {
+    const modalId = event.target.getAttribute('data-modal');
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
+
+// Agrega un evento de clic a los botones que abren el modal
+openModalButtons.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+
+// Agrega un evento de clic a los botones de cierre de los modales
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', closeModal);
+});
+
+// Agrega un evento de clic a la ventana para cerrar el modal si se hace clic fuera de él
+window.addEventListener('click', (event) => {
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
